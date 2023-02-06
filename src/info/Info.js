754fca03eedf93ff10b9b0233d7efa72c0450f39
sindiga-dev <isaac.sindiga@student.moringaschool.com>
2023-02-06 17:17:13 +0300

    Adds projects

--- src/info/Info.js
+++ src/info/Info.js
@@ -1,9 +1,9 @@
 import self from "../img/self.png"
 import mock1 from "../img/mock1.png"
 import mock2 from "../img/mock2.png"
-import mock3 from "../img/mock3.png"
-import mock4 from "../img/mock4.png"
-import mock5 from "../img/mock5.png"
+// import mock3 from "../img/mock3.png"
+// import mock4 from "../img/mock4.png"
+// import mock5 from "../img/mock5.png"
 
 /* Hi there! Thanks for checking out my portfolio template. Be sure to read the comments to get a better understanding of
 how to make this template work best for you! */
@@ -106,18 +106,18 @@ export const info = {
 // You can also remove the emojis if you'd like, I just think they look cute :P
     ],
     portfolio: [ // This is where your portfolio projects will be detailed
-        // {
-        //     title: "Project 1",
-        //     live: "https://paytonpierce.dev", //this should be a link to the live version of your project, think github pages, netlify, heroku, etc. Or your own domain, if you have it.
-        //     source: "https://github.com/paytonjewell", // this should be a link to the **repository** of the project, where the code is hosted.
-        //     image: mock1
-        // },
-        // {
-        //     title: "Project 2",
-        //     live: "https://paytonpierce.dev",
-        //     source: "https://github.com/paytonjewell",
-        //     image: mock2
-        // },
+        {
+            title: "Project 1",
+            live: "https://sindiga-dev.github.io/Recipe-finder/", //this should be a link to the live version of your project, think github pages, netlify, heroku, etc. Or your own domain, if you have it.
+            source: "https://github.com/sindiga-dev/Recipe-finder", // this should be a link to the **repository** of the project, where the code is hosted.
+            image: mock1
+        },
+        {
+            title: "Project 2",
+            live: "https://qkao-recipes.netlify.app/",
+            source: "https://github.com/sindiga-dev/Q-kao-Recipes",
+            image: mock2
+        },
         // {
         //     title: "Project 3",
         //     live: "https://paytonpierce.dev",
